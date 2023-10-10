44fad9e2f520c6b4dd05c705e3c73c9f7c26457b
Janosch Püschel <janosch.pueschel@gmail.com>
2023-10-10 23:09:48 +0200

    feat: add function to fetch weather data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const weatherLocation = ref('')
+const weatherData = ref()
+
+function getWeatherData() {
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=b3d01e05915d4c66b0f155101230508&q=${weatherLocation.value}`
+  )
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => (weatherData.value = response))
+}
+</script>
 
 <template>
-  <h1>Weather App</h1>
+  <input type="text" v-model="weatherLocation" class="border" />
+  <button @click="getWeatherData">Search</button>
+  <p>{{ weatherData }}</p>
 </template>
