9961595c00b107452722086e77f67e94d6606d0b
Janosch Püschel <janosch.pueschel@gmail.com>
2023-10-13 00:14:47 +0200

    feat: add component to toggle unit of temperature

--- src/components/CurrentWeatherCard.vue
+++ src/components/CurrentWeatherCard.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-defineProps(['currentWeatherData'])
+import UnitToggle from './UnitToggle.vue'
 
-const temperatureUnitCelsius = ref(true)
-function toggleTemperatureUnit() {
-  temperatureUnitCelsius.value = !temperatureUnitCelsius.value
-}
+defineProps(['currentWeatherData'])
 </script>
 
 <template>
   <div class="flex flex-col items-center py-5">
     <img src="//cdn.weatherapi.com/weather/64x64/day/113.png" />
     <h1>{{ currentWeatherData.location }}</h1>
-    <p>
-      {{
-        temperatureUnitCelsius
-          ? `${currentWeatherData.temp_c} °C`
-          : `${currentWeatherData.temp_f} °F`
-      }}
-    </p>
-    <button @click="toggleTemperatureUnit">Toggle Celsius-Fahrenheit</button>
+    <div>
+      <UnitToggle :current-weather-data="currentWeatherData" />
+    </div>
   </div>
 </template>
---
+++ src/components/UnitToggle.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps(['currentWeatherData'])
+
+const temperatureUnitCelsius = ref(true)
+function toggleTemperatureUnit() {
+  temperatureUnitCelsius.value = !temperatureUnitCelsius.value
+}
+</script>
+
+<template>
+  <div class="flex">
+    <span class="mr-1">{{
+      temperatureUnitCelsius ? currentWeatherData.temp_c : currentWeatherData.temp_f
+    }}</span>
+    <label class="swap">
+      <input type="checkbox" @click="toggleTemperatureUnit" />
+      <div class="swap-on">°F <span class="text-neutral-700">/ °C</span></div>
+      <div class="swap-off">°C <span class="text-neutral-700">/ °F</span></div>
+    </label>
+  </div>
+</template>
