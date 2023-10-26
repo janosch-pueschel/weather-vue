7408867ea63abe273137e31e3794e1a94a861e3a
Janosch Püschel <janosch.pueschel@gmail.com>
2023-10-26 23:52:55 +0200

    refactor: edit styling of weather forecast table

--- src/components/WeatherForecastTable.vue
+++ src/components/WeatherForecastTable.vue
@@ -3,10 +3,16 @@ defineProps(['weatherForecast'])
 </script>
 
 <template>
-g    <div v-for="weather in weatherForecast" :key="weather.index">
-        <p>{{ weather.date }}</p>
-        <p>{{ weather.temp_c }}</p>
-        <p>{{ weather.temp_f}}</p>
-        <p>{{ weather.icon }}</p>
+  <div v-for="weather in weatherForecast" :key="weather.index" class="grid grid-cols-3">
+    <div class="flex justify-center items-center">
+      <p>{{ weather.date }}</p>
     </div>
+    <div class="flex justify-center items-center">
+      <img :src="weather.icon">
+    </div>
+    <div class="flex justify-center items-center">
+      <p>{{ weather.temp_c }} °C</p>
+      <p>{{ weather.temp_f }} °F</p>
+    </div>
+  </div>
 </template>
