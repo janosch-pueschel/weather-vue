370dcf676e8b8af03092530743ba7659fa192395
Janosch Püschel <janosch.pueschel@gmail.com>
2023-11-05 00:49:50 +0100

    refactor: update styling of WeatherForecastTable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      gridTemplateColumns: {
+        table: '1fr 1fr 3fr'
+      },
+      borderWidth: {
+        1: '1px'
+      }
+    }
   },
   plugins: [require('daisyui')]
 }
