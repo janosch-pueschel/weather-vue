002f2067ebb4861af4da7a648ee6be82f6f95ee3
Janosch Püschel <janosch.pueschel@gmail.com>
2024-01-05 12:37:30 +0100

    fix: update unittoggle to sync with temperature unit

--- src/components/UnitToggle.vue
+++ src/components/UnitToggle.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-defineProps(['temperatureUnit'])
+const props = defineProps(['temperatureUnit'])
 </script>
 
 <template>
   <div class="flex">
     <label class="swap">
-      <input type="checkbox" @click="$emit('toggleTemperatureUnit')" />
-      <div class="swap-on"><span class="text-neutral-700">°C /</span> °F</div>
-      <div class="swap-off">°C <span class="text-neutral-700">/ °F</span></div>
+      <input
+        type="checkbox"
+        @click="$emit('toggleTemperatureUnit')"
+        :checked="props.temperatureUnit ? false : true"
+      />
+      <div class="swap-on font-medium"><span class="text-neutral-700 font-thin">°C /</span> °F</div>
+      <div class="swap-off font-medium">
+        °C <span class="text-neutral-700 font-thin">/ °F</span>
+      </div>
     </label>
   </div>
 </template>
